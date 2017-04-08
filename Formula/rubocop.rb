f97309b28aec3ca6055625feab5d2816e43c9ab7
rockyluke <rockyluke@offline.net>
2017-04-08 15:26:06 +0200

    Update formulas

--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -3,7 +3,7 @@ class Rubocop < Formula
   version "0.48.1"
   homepage "https://github.com/bbatsov/rubocop"
   url "https://rubygems.org/gems/rubocop"
-  sha256 "002f6b49013abdc05c68ae75433c48d3ee7f1baa70674d60bf1cc310e210fbd7"
+  sha256 "1781fa5192d1a65d241f7deb504fc147534bae3282c9160f4875f02c57dd490f"
 
   depends_on "brew-gem" => :build
 
