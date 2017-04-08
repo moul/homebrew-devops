c430968fd801bbb952b04358ec19f708f54d8cff
rockyluke <rockyluke@offline.net>
2017-04-08 15:19:27 +0200

    Update formulas

--- Formula/puppet-lint.rb
+++ Formula/puppet-lint.rb
@@ -1,6 +1,6 @@
 class PuppetLint < Formula
   desc "Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies."
-  homepage "https://rubygems.org/gems/puppet-lint"
+  homepage "https://github.com/rodjek/puppet-lint"
   url "https://rubygems.org/gems/puppet-lint"
   sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
 
--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -1,6 +1,6 @@
 class Rubocop < Formula
   desc "Automatic Ruby code style checking tool. Aims to enforce the community-driven Ruby Style Guide."
-  homepage "https://rubygems.org/gems/rubocop"
+  homepage "https://github.com/bbatsov/rubocop"
   url "https://rubygems.org/gems/rubocop"
   sha256 "002f6b49013abdc05c68ae75433c48d3ee7f1baa70674d60bf1cc310e210fbd7"
 
