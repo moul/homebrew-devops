2c4e43f49fe8ee55d0a2832d77380eae2f2b290c
rockyluke <rockyluke@offline.net>
2017-04-09 16:23:03 +0200

    Update formulas

--- Formula/puppet-lint.rb
+++ Formula/puppet-lint.rb
@@ -4,15 +4,12 @@ class PuppetLint < Formula
   url "https://rubygems.org/gems/puppet-lint/"
   sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
 
-  def initialize(*args)
-    @name = "puppet-lint"
-    @version = "2.2.1"
-    @prefix = "#{lib}/gems"
-  end
-
   def install
-    ENV["GEM_HOME"] = "#{lib}/gems"
-    system "gem", "install", name, "--version", version, "--install-dir", prefix
+    system "gem",
+           "install", "puppet-lint",
+           "--version", "2.2.1",
+           "--install-dir", "#{lib}/gems",
+           "--bindir", "#{bin}"
   end
 
   test do
