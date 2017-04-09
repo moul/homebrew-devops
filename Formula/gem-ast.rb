7ba7eccf42611991b70f44673fcd4258819dc06b
rockyluke <rockyluke@offline.net>
2017-04-09 17:45:16 +0200

    Update formulas

--- Formula/gem-ast.rb
+++ Formula/gem-ast.rb
@@ -5,11 +5,9 @@ class GemAst < Formula
   sha256 "15d97cf7f3430351a8663f2c5fb7591fb29f700fa28576c46c53b992e912e85e"
 
   def install
-    ENV["PATH"] = "#{bin}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
     system "gem",
            "install", "ast",
            "--version", "2.3.0",
-           "--bindir", "#{bin}",
            "--user-install"
   end
 end
--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -4,11 +4,11 @@ class Rubocop < Formula
   url "https://rubygems.org/gems/rubocop-0.48.1.gem"
   sha256 "002f6b49013abdc05c68ae75433c48d3ee7f1baa70674d60bf1cc310e210fbd7"
 
-  depends_on "gem-parser"
-  depends_on "gem-powerpack"
-  depends_on "gem-rainbow"
-  depends_on "gem-ruby-progressbar"
-  depends_on "gem-unicode-display-width"
+  depends_on "gem-parser" :install
+  depends_on "gem-powerpack" :install
+  depends_on "gem-rainbow" :install
+  depends_on "gem-ruby-progressbar" :install
+  depends_on "gem-unicode-display-width" :install
 
   def install
     ENV["PATH"] = "#{bin}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
