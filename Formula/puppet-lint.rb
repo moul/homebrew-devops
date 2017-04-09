87f76a6f67001f9cb33328d601dccf1d282fa6da
rockyluke <rockyluke@offline.net>
2017-04-09 22:31:14 +0200

    Update formulas

---
+++ Formula/puppet-lint.rb
@@ -0,0 +1,11 @@
+# -*- ruby -*-
+
+class GemPuppetLint < Formula
+  desc ""
+  homepage "https://github.com/rodjek/puppet-lint/"
+  url "https://rubygems.org/gems/puppet-lint-2.2.1.gem"
+  sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
+
+  ohai "Please run brew install puppet-lint"
+end
+# EOF
---
+++ Formula/rubocop.rb
@@ -0,0 +1,11 @@
+# -*- ruby -*-
+
+class Rubocop < Formula
+  desc ""
+  homepage "https://github.com/bbatsov/rubocop/"
+  url "https://rubygems.org/gems/rubocop-0.48.1.gem"
+  sha256 "002f6b49013abdc05c68ae75433c48d3ee7f1baa70674d60bf1cc310e210fbd7"
+
+  ohai "Please run brew install gem-rubocop"
+end
+# EOF
