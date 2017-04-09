03da85585a15bcc4225ac3756fd1c3f285d3b056
rockyluke <rockyluke@offline.net>
2017-04-09 17:55:21 +0200

    Update formulas

--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -4,11 +4,11 @@ class Rubocop < Formula
   url "https://rubygems.org/gems/rubocop-0.48.1.gem"
   sha256 "002f6b49013abdc05c68ae75433c48d3ee7f1baa70674d60bf1cc310e210fbd7"
 
-  depends_on "gem-parser" => :install
-  depends_on "gem-powerpack" => :install
-  depends_on "gem-rainbow" => :install
-  depends_on "gem-ruby-progressbar" => :install
-  depends_on "gem-unicode-display-width" => :install
+  depends_on "gem-parser" => :run
+  depends_on "gem-powerpack" => :run
+  depends_on "gem-rainbow" => :run
+  depends_on "gem-ruby-progressbar" => :run
+  depends_on "gem-unicode-display-width" => :run
 
   def install
     ENV["PATH"] = "#{bin}:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
