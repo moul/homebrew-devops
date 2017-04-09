02f7dfac047c8998dcbbe3e605e53457ee631eb8
rockyluke <rockyluke@offline.net>
2017-04-09 15:38:27 +0200

    Update formulas

--- Formula/puppet-lint.rb
+++ Formula/puppet-lint.rb
@@ -1,20 +1,24 @@
 class PuppetLint < Formula
   desc "Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies."
-  version "2.2.1"
   homepage "https://github.com/rodjek/puppet-lint"
-  url "https://rubygems.org/gems/puppet-lint-2.2.1.gem"
-  sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
+  # url "https://rubygems.org/gems/puppet-lint-2.2.1.gem"
+  # sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
+  head 'https://github.com/rodjek/puppet-lint', :using => :git, :tag => '2.2.1'
 
-  depends_on "brew-gem" => :build
+  depends_on "brew-gem"
+  depends_on "ruby"
 
-  def initialize(*args)
-    @name = "puppet-lint"
-    @version = "2.2.1"
-    @prefix = #{prefix}
-    super
-  end
+  # def initialize(*args)
+  #   @name = "puppet-lint"
+  #   @version = "2.2.1"
+  #   @prefix = #{prefix}
+  #   super
+  # end
 
   def install
-    system "gem", "install", name, "--version", version, "--install-dir", prefix
+    # ENV["GEM_HOME"] = libexec
+    # system "gem", "install", name, "--version", version, "--install-dir", prefix
+    system "gem", "build", "puppet-lint.gemspec"
+    system "gem", "install", "puppet-lint-2.2.1.gem"
   end
 end
