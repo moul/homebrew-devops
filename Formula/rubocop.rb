da16d28a0f4dfc6545671cde42439064f81e7211
rockyluke <rockyluke@offline.net>
2017-04-09 16:39:34 +0200

    Update formulas

--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -1,7 +1,7 @@
 class Rubocop < Formula
   desc "Automatic Ruby code style checking tool. Aims to enforce the community-driven Ruby Style Guide."
   homepage "https://github.com/bbatsov/rubocop/"
-  url "https://rubygems.org/gems/rubocop/"
+  url "https://rubygems.org/gems/rubocop-0.48.1.gem"
   sha256 "1781fa5192d1a65d241f7deb504fc147534bae3282c9160f4875f02c57dd490f"
 
   def install
@@ -9,6 +9,11 @@ class Rubocop < Formula
            "install", "rubocop",
            "--version", "0.48.1",
            "--bindir", "#{bin}",
-           "--user-install"
+           "--user-install",
+           "--conservative"
+  end
+
+  test do
+    system "#{bin}/rubocop --help"
   end
 end
