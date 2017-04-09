fd290bed6e8b74bcdd7c9353915dab4fa255e637
rockyluke <rockyluke@offline.net>
2017-04-09 16:19:25 +0200

    Update formulas

--- Formula/puppet-lint.rb
+++ Formula/puppet-lint.rb
@@ -1,17 +1,17 @@
 class PuppetLint < Formula
   desc "Checks your Puppet manifests against the Puppetlabs style guide and alerts you to any discrepancies."
-  homepage "https://github.com/rodjek/puppet-lint"
-  url "https://rubygems.org/gems/puppet-lint"
+  homepage "https://github.com/rodjek/puppet-lint/"
+  url "https://rubygems.org/gems/puppet-lint/"
   sha256 "f7f191e8bcd8ff35744bd5197548d283ecc1e117e08b6a3f0e614cb306140d87"
 
   def initialize(*args)
     @name = "puppet-lint"
     @version = "2.2.1"
-    @prefix = #{lib}/gems
+    @prefix = "#{lib}/gems"
   end
 
   def install
-    ENV["GEM_HOME"] = #{lib}/gems
+    ENV["GEM_HOME"] = "#{lib}/gems"
     system "gem", "install", name, "--version", version, "--install-dir", prefix
   end
 
--- Formula/rubocop.rb
+++ Formula/rubocop.rb
@@ -1,17 +1,13 @@
 class Rubocop < Formula
   desc "Automatic Ruby code style checking tool. Aims to enforce the community-driven Ruby Style Guide."
-  version "0.48.1"
-  homepage "https://github.com/bbatsov/rubocop"
-  url "https://rubygems.org/gems/rubocop-0.48.1.gem"
+  homepage "https://github.com/bbatsov/rubocop/"
+  url "https://rubygems.org/gems/rubocop/"
   sha256 "1781fa5192d1a65d241f7deb504fc147534bae3282c9160f4875f02c57dd490f"
-
-  depends_on "brew-gem" => :build
 
   def initialize(*args)
     @name = "rubocop"
     @version = "0.48.1"
-    @prefix = #{prefix}
-    super
+    @prefix = #{lib}/gems
   end
 
   def install
